055dd771f046499e05ba1719db7d5f33ff4aee75
Erxhan Selmani <erxhantooddoc@gmail.com>
2021-11-22 20:53:55 +0100

    Types added

---
+++ types/index.ts
@@ -0,0 +1,29 @@
+export type Data = {
+  'patient-cases': PatientCase[];
+  reports: Report[];
+  'medical-doctors': Doctor[];
+}
+
+export type PatientCase = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  country: string;
+  sex: 'M' | 'F';
+  'cancer-type': string;
+  'medical-doctor-id': number;
+  age: number;
+}
+
+export type Report = {
+  id: number;
+  'patient-id': number;
+  status: 'available' | 'unavailable';
+  'creation-date': string;
+}
+
+export type Doctor = {
+  id: number;
+  firstname: string;
+  lastname: string;
+}
