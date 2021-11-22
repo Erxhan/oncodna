642d74129a98288f8e8c0a27e31250bd80f67cce
Erxhan Selmani <erxhantooddoc@gmail.com>
2021-11-22 20:54:15 +0100

    Custom Icon (svg)

---
+++ components/Icons/index.tsx
@@ -0,0 +1,12 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const ChevronIcon = createIcon({
+  displayName: "ChevronIcon",
+  viewBox: "0 0 30 31",
+  path: (
+    <path
+      fill="#000"
+      d="M10.5 13.3c0-.2.1-.4.2-.5.3-.3.8-.3 1.1 0l3 3 3-3c.3-.3.8-.3 1.1 0 .3.3.3.8 0 1.1l-4 4-4-4c-.3-.3-.4-.5-.4-.6z"
+    />
+  ),
+});
